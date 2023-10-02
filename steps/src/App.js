c3ae94542ff237a357fa917d2af108dc35785b4c
SrestMilan <shrestham780@gmail.com>
2023-10-02 07:31:39 +0545

    working on state

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const messages = [
   "Learn React ⚛️",
@@ -7,32 +7,32 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
   const style = { backgroundColor: "#7950f2", color: "#fff" };
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
-    alert("Previous");
+    if (step > 1) setStep(step - 1);
   }
 
   function handleNext() {
-    alert("Next");
+    if (step < 3) setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : " "}`}>1</div>
-        <div className={`${step >= 2 ? "active" : " "}`}>2</div>
-        <div className={`${step >= 3 ? "active" : " "}`}>3</div>
+        <div className={step >= 1 ? "active" : " "}>1</div>
+        <div className={step >= 2 ? "active" : " "}>2</div>
+        <div className={step >= 3 ? "active" : " "}>3</div>
       </div>
       <p className="message">
-        Step {step}:{messages[step - 2]}
+        Steps {step}:{messages[step - 1]}
       </p>
       <div className="buttons">
         <button style={style} onClick={handlePrevious}>
           Previous
         </button>
-        <button style={style} onMouseEnter={handleNext}>
+        <button style={style} onClick={handleNext}>
           Next
         </button>
       </div>
